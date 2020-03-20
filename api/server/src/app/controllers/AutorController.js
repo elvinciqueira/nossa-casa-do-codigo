bceb6a887ef267efe7aba8567d7feb259416bf02
elvinciqueira <elvinciqueira@gmail.com>
2020-03-20 20:11:58 -0300

    feat: cadastro de um novo autor

--- api/server/src/app/controllers/AutorController.js
+++ api/server/src/app/controllers/AutorController.js
@@ -1,7 +1,25 @@
+import * as Yup from 'yup';
 import Autor from '../models/Autor';
 
 class AutorController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      descricao: Yup.string()
+        .required()
+        .test(
+          'A descrição só pode conter 400 caracteres',
+          descricao => descricao.length === 400
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validação falhou.' });
+    }
+
     const { email } = req.body;
 
     const checkEmail = await Autor.findOne({ where: { email } });
@@ -10,9 +28,13 @@ class AutorController {
       return res.status(400).json({ error: 'email duplicado.' });
     }
 
-    const autor = await Autor.create(req.body);
+    const { nome, descricao } = await Autor.create(req.body);
 
-    return res.json(autor);
+    return res.json({
+      nome,
+      email,
+      descricao,
+    });
   }
 }
 
