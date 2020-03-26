cb8162941d6d1577da077b80ca6cda12bde10351
elvinciqueira <elvinciqueira@gmail.com>
2020-03-26 00:59:56 -0300

    fix: middleware de validação do nome

--- api/server/src/routes.js
+++ api/server/src/routes.js
@@ -5,11 +5,17 @@ import CategoriaController from './app/controllers/CategoriaController';
 
 import validateAutorStore from './app/validators/AutorStore';
 import validateCategoriaCreate from './app/validators/CategoriaCreate';
+import validateNomeCategoria from './app/validators/CheckNomeCategoria';
 
 const routes = new Router();
 
 routes.post('/autores', validateAutorStore, AutorController.store);
 
-routes.post('/categoria', validateCategoriaCreate, CategoriaController.create);
+routes.post(
+  '/categoria',
+  validateCategoriaCreate,
+  validateNomeCategoria,
+  CategoriaController.create
+);
 
 export default routes;
